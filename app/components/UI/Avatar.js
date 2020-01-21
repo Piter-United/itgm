6293627601256c4f1928c9c65d20886cd6ceb69a
aleksey-ilin <ilinalekseys@gmail.com>
2020-01-21 18:19:28 +0300

    add default type to Avatar

--- app/components/UI/Avatar.js
+++ app/components/UI/Avatar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-const size = { participant: '80px', partner: '180px' }
+const size = { default: '80px', participant: '80px', partner: '180px' }
 
-const Avatar = ({ type, src, alt }) => (
-  <img
-    src={src}
-    alt={alt}
-    style={{
-      width: size[type],
-      height: size[type],
-      borderRadius: '50%'
-    }}
-  />
-)
+const Avatar = ({ type, src, alt }) => {
+  const avatarSize = size[type] ? size[type] : size.default
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      style={{
+        width: avatarSize,
+        height: avatarSize,
+        borderRadius: '50%'
+      }}
+    />
+  )
+}
 
 export default Avatar

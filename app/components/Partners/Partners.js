8a00313c4ff53b3a1b0eaf8cacdaef7a621cb9b2
enmalafeev <malafeev.en@yandex.ru>
2020-01-21 23:01:54 +0300

    rename Partners component

--- app/components/Partners/Partners.js
+++ app/components/Partners/Partners.js
@@ -1,43 +1,45 @@
 import React from 'react'
 import './Partners.css'
 
-const Partners = () => {
-  const partners = [
-    {
-      name: 'Important Partner Name',
-      photo: 'img.jpeg',
-      desc:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-      name: 'Important Partner Name',
-      photo: 'img.jpeg',
-      desc:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    }
-  ]
-  const renderPartners = () => (
-    <ul className="partners__list">
-      {partners.map(partner => (
-        <li className="partners-item">
-          <div className="partner-cnt">
-            <img src={partner.img} alt="" className="partners-item__img" />
-          </div>
-          <h3 className="partners-item__header">{partner.name}</h3>
-          <p className="partners-item__desc">{partner.desc}</p>
-        </li>
-      ))}
-    </ul>
-  )
+const partners = [
+  {
+    name: 'Important Partner Name',
+    photo: 'img.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    name: 'Important Partner Name',
+    photo: 'img.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  }
+]
+
+const Partner = () => (
+  <ul className="partners__list">
+    {partners.map(partner => (
+      <li className="partners-item">
+        <div className="partner-cnt">
+          <img src={partner.img} alt="" className="partners-item__img" />
+        </div>
+        <h3 className="partners-item__header">{partner.name}</h3>
+        <p className="partners-item__desc">{partner.desc}</p>
+      </li>
+    ))}
+  </ul>
+)
+
+const PartnerList = () => {
   return (
     <div className="container">
       <div className="header-wrapper">
         <h2 className="partners__header">Партнеры</h2>
-        <span className="partners__count">(8)</span>
+        <span className="partners__count">({partners.length})</span>
       </div>
-      {renderPartners()}
+      {Partner()}
     </div>
   )
 }
 
-export default Partners
+export default PartnerList

411451f11a4020c9b443fd86806a502d97dd8daf
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-01-22 15:47:22 +0300

    Create layout for home page

---
+++ app/components/HomePage/HomePage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import Hero from '../Hero/Hero'
+import Header from '../Header'
+import PageSection from '../PageSection/PageSection'
+import ContentContainer from '../ContentContainer/ContentContainer'
+import Footer from '../Footer/Footer'
+import ActivityList from '../Activity/ActivityList'
+
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className="out">
+      <Hero />
+      <Header />
+
+      <PageSection>
+        <ContentContainer>
+          <ActivityList />
+        </ContentContainer>
+      </PageSection>
+
+      <PageSection style={{ background: 'lightgray' }}>
+        <ContentContainer>
+          <p>PageSection</p>
+        </ContentContainer>
+      </PageSection>
+
+      <PageSection style={{ background: 'gray' }}>
+        <ContentContainer>
+          <p>PageSection</p>
+        </ContentContainer>
+      </PageSection>
+
+      <PageSection style={{ background: 'lightgray' }}>
+        <ContentContainer>
+          <p>PageSection</p>
+        </ContentContainer>
+      </PageSection>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default HomePage

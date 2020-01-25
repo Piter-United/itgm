e0d45c4657c51a9eb2b405cf99c1c081bf2584c5
enmalafeev <malafeev.en@yandex.ru>
2020-01-25 23:44:10 +0300

    add Header component

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
+import './Header.css'
+import Logo from '../Logo/Logo'
+
+const linkList = ['Главная', 'Программа', 'Сообщества', 'Участники', 'Партнеры']
+
+const HeaderList = () => {
+  return (
+    <ul className="Header-List">
+      {linkList.map(link => (
+        <li className="Header-Item" key={link}>
+          <a className="Header-Link">{link}</a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 const Header = () => {
-  return <h1>Header</h1>
+  return (
+    <div className="Header">
+      <div className="Header-Cnt">
+        <Logo />
+        {HeaderList()}
+        {/* <div className="Header-Menu"></div> */}
+        <div className="Header-UserMenu">
+          <button type="button" className="Header-Search" />
+          <button type="button" className="Header-Notification" />
+          <button type="button" className="Header-Avatar" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header

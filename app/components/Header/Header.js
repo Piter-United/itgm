60cb140669740911ef2463f3ca0fae5305e9a0e2
alexsuslov <suslov@me.com>
2020-01-25 14:12:31 +0300

    header tester

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -5,6 +5,8 @@ import { Layout, Menu } from 'antd'
 import { LOGOUT } from '../../store/user'
 
 import Logo from '../Logo/Logo'
+import Mainmenu from '../Mainmenu/Mainmenu'
+import UserMenu from '../usermenu/usermenu'
 import Burger from '../Burger/Burger'
 
 import '../main-nav.css'
@@ -21,105 +23,30 @@ const hasMatchRoute = (path, url, exact = true) => {
   return m && 'path' in m
 }
 
-export default withRouter(
-  ({
-    history: {
-      location: { pathname: path }
-    }
-  }) => {
-    const { userId, user, dispatch } = useStoreon('user', 'userId')
-    const themeMenu = hasMatchRoute(path, '/') ? 'dark' : null
+export default props => {
+  const { theme } = props
+  // const { userId, user, dispatch } = useStoreon('user', 'userId')
+  // const themeMenu = hasMatchRoute(path, '/') ? 'dark' : null
 
-    const [isMenuOpen, openMenu] = useState(false)
+  const [isMenuOpen, openMenu] = useState(false)
 
-    const clickOnBurger = () => {
-      openMenu(!isMenuOpen)
-      const body = document.body
-      body.classList.toggle('menu-is-open')
-    }
-
-    return (
-      <Header className="main-nav">
-        <div className="main-nav__logo">
-          <Logo theme={themeMenu} />
-        </div>
-        <div className={`main-nav__menu ${isMenuOpen ? 'is-open' : ''}`}>
-          <Menu theme={themeMenu} selectable={false} className="main-menu">
-            <Menu.Item
-              className={
-                hasMatchRoute(path, '/') ? 'ant-menu-item-selected' : ''
-              }
-              key="1"
-            >
-              <Link to="/">Главная</Link>
-            </Menu.Item>
-            <Menu.Item
-              className={
-                hasMatchRoute(path, '/community', false)
-                  ? 'ant-menu-item-selected'
-                  : ''
-              }
-              key="2"
-            >
-              <Link to="/community">Сообщества</Link>
-            </Menu.Item>
-            {!userId && (
-              <Menu.Item
-                key="97"
-                className={
-                  hasMatchRoute(path, '/login') ? 'ant-menu-item-selected' : ''
-                }
-              >
-                <Link to="/login">Вход</Link>
-              </Menu.Item>
-            )}
-            {userId && [
-              <Menu.Item
-                key="98"
-                className={
-                  hasMatchRoute(path, '/user') ? 'ant-menu-item-selected' : ''
-                }
-              >
-                {user ? (
-                  <Link to="/user">{user.name}</Link>
-                ) : (
-                  <Link to="/user/edit">
-                    {user ? user.name : 'Заполните профиль'}
-                  </Link>
-                )}
-              </Menu.Item>,
-              <Menu.Item
-                key="99"
-                className={
-                  hasMatchRoute(path, '/logout') ? 'ant-menu-item-selected' : ''
-                }
-              >
-                <a type="link" onClick={() => dispatch(LOGOUT)}>
-                  Выход
-                </a>
-              </Menu.Item>
-            ]}
-          </Menu>
-        </div>
-
-        <div className="main-nav__user-menu">
-          <button
-            type="button"
-            className="main-nav__search main-nav__search--theme-dark"
-          />
-          <button
-            type="button"
-            className="main-nav__notification main-nav__notification--theme-dark"
-          />
-          <button
-            type="button"
-            className="main-nav__avatar main-nav__avatar--theme-dark"
-          />
-        </div>
-        <div className="main-nav__menu-switcher">
-          <Burger theme="dark" onClick={clickOnBurger} />
-        </div>
-      </Header>
-    )
+  const clickOnBurger = () => {
+    openMenu(!isMenuOpen)
+    const body = document.body
+    body.classList.toggle('menu-is-open')
   }
-)
+
+  return (
+    <Header className="main-nav">
+      <div className="main-nav__logo">
+        <Logo theme={theme} />
+      </div>
+      <Mainmenu {...props} />
+      <UserMenu {...props} />
+
+      <div className="main-nav__menu-switcher">
+        <Burger theme="dark" onClick={clickOnBurger} />
+      </div>
+    </Header>
+  )
+}
--- app/pages/trying_header/index.js
+++ app/pages/trying_header/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 
+const style = {
+  backgroundColor: 'gray'
+}
 /*
 
 */
 const Trying = props => (
   <div>
     <h3>Main header</h3>
-    <Header {...props} theme="dark" />
+    <div style={style}>
+      <Header {...props} theme="dark" />
+    </div>
     <h3>light header</h3>
     <Header {...props} />
   </div>

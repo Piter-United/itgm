ee4ba187ac820aab49e8fcbe6e338a2209d0694c
Eldar Babaev <el.glagius@gmail.com>
2020-01-28 14:37:56 +0700

    Created new component

---
+++ app/components/Community/CommunityBadgeList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Tooltip } from 'antd'
+import './CommunityBadgeList.css'
+
+const CommunityBadgeList = ({ list: communities }) => {
+  return (
+    <Card bordered={false} className="Community-Badge-List">
+      {communities.map(community => {
+        const isPrimary = community.primary
+        return (
+          <Tooltip placement="top" key={community.name} title={community.name}>
+            <Button
+              href={`./community/${community.id}`}
+              className="Community-Badge-List__badge"
+              style={{
+                border: '1px solid #6F4297',
+                backgroundColor: isPrimary ? '#6F4297' : 'white',
+                color: isPrimary ? 'white' : '#6F4297'
+              }}
+            >
+              {community.name}
+            </Button>
+          </Tooltip>
+        )
+      })}
+    </Card>
+  )
+}
+
+export default CommunityBadgeList

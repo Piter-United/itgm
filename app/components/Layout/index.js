d9cbfe37f5f801396a9cd88ecb7a3096d40a330b
Eldar Babaev <el.glagius@gmail.com>
2020-02-01 17:27:06 +0700

    changed Layout classname (request from PR comment)

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -4,7 +4,7 @@ import { Footer } from '../Footer'
 import './style.css'
 
 const Layout = ({ theme = 'default', component: Component, ...props }) => (
-  <div className="out">
+  <div className="Layout">
     <Header theme={theme} />
     <Component {...props} />
     <Footer theme={theme} />

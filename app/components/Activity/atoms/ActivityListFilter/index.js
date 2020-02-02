866b0af97906d98bd7985089b5ba4f4d1bf61cac
Zipofar <zipofar@gmail.com>
2020-02-02 16:46:53 +0300

    Update css activity item filter block

--- app/components/Activity/atoms/ActivityListFilter/index.js
+++ app/components/Activity/atoms/ActivityListFilter/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import useStoreon from 'storeon/react'
+import cn from 'classnames'
 import { Button, Form, Input, Icon, Row } from 'antd'
 
 import { GET_LIST as GET_LIST_COMMUNITY } from 'store/community'
@@ -13,9 +14,13 @@ const ActivityFilter = ({ handleClose }) => {
   }, [dispatch])
 
   const btnsCommunities = community.list.map(e => {
+    const isSelectedCommunity = e.id === activity.community
     return (
       <Button
-        type={e.id === activity.community ? 'primary' : ''}
+        className={cn({
+          'ActivityFilter-Control': true,
+          'ActivityFilter-Control_selected': isSelectedCommunity
+        })}
         onClick={() => dispatch(ON_COMMUNITY, e.id)}
         key={e.id}
       >
@@ -26,42 +31,59 @@ const ActivityFilter = ({ handleClose }) => {
 
   const btnsTags = activity.list
     .map(e => e.resource.tags)
-
     .flat()
-    .map((e, i) => (
-      <Button
-        type={activity.tags.indexOf(e) === -1 ? '' : 'primary'}
-        onClick={() => dispatch(ON_TAG, e)}
-        key={i + 'tag'}
-      >
-        {e}
-      </Button>
-    ))
+    .map((e, i) => {
+      const isSelectedTag = activity.tags.indexOf(e) > -1
+      return (
+        <Button
+          className={cn({
+            'ActivityFilter-Control': true,
+            'ActivityFilter-Control_selected': isSelectedTag
+          })}
+          onClick={() => dispatch(ON_TAG, e)}
+          key={i + 'tag'}
+        >
+          {e}
+        </Button>
+      )
+    })
 
   return (
     <div className="ActivityFilter-Content">
       <Row type="flex" justify="end">
         <Icon
-          className="ActivityPage-BtnCloseFilter"
+          className="ActivityFilter-BtnCloseFilter"
           type="close"
           style={{ fontSize: '20px' }}
           onClick={handleClose}
         />
       </Row>
-      <span>Поиск по темам</span>
-      <Form className="login-form">
-        <Form.Item>
-          <Input
-            defaultValue={activity.filter}
-            onChange={({ target }) => dispatch(ON_FILTER, target.value)}
-            prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          />
-        </Form.Item>
-      </Form>
-      <span>Сообщества</span>
-      <div>{btnsCommunities}</div>
-      <span>Метки</span>
-      <div>{btnsTags}</div>
+      <div className="ActivityFilter-FilterControlBlock">
+        <span className="ActivityFilter-FilterControlLabel">
+          Поиск по темам
+        </span>
+        <Form className="login-form">
+          <Form.Item>
+            <Input
+              defaultValue={activity.filter}
+              onChange={({ target }) => dispatch(ON_FILTER, target.value)}
+              prefix={
+                <Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />
+              }
+            />
+          </Form.Item>
+        </Form>
+      </div>
+      <div className="ActivityFilter-FilterControlBlock">
+        <span className="ActivityFilter-FilterControlLabel">Сообщества</span>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {btnsCommunities}
+        </div>
+      </div>
+      <div className="ActivityFilter-FilterControlBlock">
+        <span className="ActivityFilter-FilterControlLabel">Метки</span>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>{btnsTags}</div>
+      </div>
     </div>
   )
 }

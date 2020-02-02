f60079d2f1eb766bd3ca9ee847353077fcdb67b5
Eldar Babaev <el.glagius@gmail.com>
2020-02-03 06:12:06 +0700

    Changed classnames and removed uniq component

--- app/components/UserProfile/index.js
+++ app/components/UserProfile/index.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState, Fragment } from 'react'
 import useStoreon from 'storeon/react'
-import { Spin, Typography, Divider } from 'antd'
+import { Spin, Divider } from 'antd'
 
 import { GET_CURRENT_USER } from 'store/user'
-import { GET_LIST, LIKE, UNLIKE } from 'store/activity'
+import { GET_LIST } from 'store/activity'
 
 import history from '../../history'
 
 import ProfileInfo from './atoms/ProfileInfo'
-import UserActivities from './atoms/UserActivities'
+import ActivityList from 'components/Activity/atoms/ActivityList'
 import CommunityBadgeList from 'components/Community/CommunityBadgeList'
 
 import './style.css'
-
-const { Paragraph } = Typography
 
 const UserProfile = () => {
   const { userId, user, activity, dispatch } = useStoreon(
@@ -57,24 +55,23 @@ const UserProfile = () => {
       : 'Пока еще не выбрал тему обсуждения'
   const communityList = { community, communities }
   const ActivitiesHeader = ({ text }) => (
-    <p className="UserProfile__text">{text}</p>
+    <p className="UserProfile-Text">{text}</p>
   )
 
   const activitiesProps = {
-    activity,
-    activities: userActivities,
+    activitiesData: { ...activity, list: userActivities },
     userId,
     dispatch
   }
   return (
     <main className="UserProfile">
-      <section className="UserProfile_section">
+      <section className="UserProfile-Section">
         <ProfileInfo {...user} />
         <Divider style={{ border: '1px solid #ABABAB', margin: '0' }} />
         <CommunityBadgeList {...communityList} />
         <Divider style={{ border: '1px solid #ABABAB', margin: '0' }} />
         <ActivitiesHeader text={userActivitiesTitle} />
-        {userActivities.length > 0 && <UserActivities {...activitiesProps} />}
+        {userActivities.length > 0 && <ActivityList {...activitiesProps} />}
       </section>
     </main>
   )

030e28c6ff1fdc6ff72834d7c2dfbb0839efb570
suchimauz <alexey.fedulov98@gmail.com>
2020-02-02 20:26:03 +0300

    Participant List, add Filter and Search

--- app/store/participant.js
+++ app/store/participant.js
@@ -1,44 +1,24 @@
 const defaultState = {
   filter: '',
+  community: '',
+  loading: false,
   sort: false,
-  list: [
-    {
-      id: 1,
-      avatar: 'img.jpeg',
-      firstName: 'Иван',
-      lastName: 'Иванов',
-      community: 'PiterJS'
-    },
-    {
-      id: 2,
-      avatar: 'img.jpeg',
-      firstName: 'Иван',
-      lastName: 'Иванов',
-      community: 'SPb Python Community'
-    },
-    {
-      id: 3,
-      avatar: 'img.jpeg',
-      firstName: 'Иван',
-      lastName: 'Иванов',
-      community: 'UX Analytics'
-    }
-  ]
+  list: []
 }
 
 export const GET_LIST = 'participant/get-list'
 export const SET_LIST = 'participant/set-list'
 export const ERROR = 'participant/error'
 export const LOADING = 'participant/loading'
-
+export const ON_FILTER = 'participant/on-filter'
+export const ON_COMMUNITY = 'participant/on-community'
 const participant = store => {
   store.on('@init', store => ({ ...store, participant: defaultState }))
   store.on(GET_LIST, s => {
     store.dispatch('request', {
-      resourceType: '$query',
-      id: 'userinfo',
+      resourceType: 'UserProfile',
       params: {
-        user: s.userId
+        _assoc: 'community'
       },
       success: SET_LIST,
       error: ERROR,
@@ -46,22 +26,40 @@ const participant = store => {
     })
   })
   store.on(SET_LIST, (state, data) => {
-    // TODO: make request to backend for return sorted participants
-    const participantListByNewest = [...data.data].sort(
-      ({ ts: ts1 }, { ts: ts2 }) => {
-        const date1 = new Date(ts1)
-        const date2 = new Date(ts2)
-        if (date1 > date2) return -1
-        if (date2 < date1) return 1
-        return 0
-      }
-    )
     return {
-      participant: { ...state.participant, list: participantListByNewest }
+      participant: {
+        ...state.participant,
+        list: data.entry
+          .map(v => v.resource)
+          .map(v => ({
+            id: v.id,
+            name: v.name,
+            community: {
+              name: v.community.resource.name,
+              id: v.community.id
+            },
+            avatar: 'img.jpeg'
+          }))
+      }
     }
   })
+
   store.on(LOADING, (state, loading) => {
-    return { activity: { ...state.activity, loading } }
+    return { participant: { ...state.participant, loading } }
+  })
+
+  store.on(ON_FILTER, (state, filter) => {
+    return { ...state, participant: { ...state.participant, filter } }
+  })
+
+  store.on(ON_COMMUNITY, (state, community) => {
+    return {
+      ...state,
+      participant: {
+        ...state.participant,
+        community: community === state.participant.community ? '' : community
+      }
+    }
   })
 }
 
